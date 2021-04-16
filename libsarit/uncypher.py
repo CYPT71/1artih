435da962e6b379f48e859f089f8eafcec197a025
cyprien <cyprientaib1@gmail.com>
2021-04-16 20:15:12 +0200

    che pa

--- libsarit/uncypher.py
+++ libsarit/uncypher.py
@@ -1,26 +1,61 @@
 def FoundLetters(a, b, square):
-    
-    lettreX = 0
-    lettreY = 0
-    tmp = 0
-    for e in square:
-        if a in e:
-            lettreX = e.index(a)
-        tmp+= 1
-        if b in e:
-            lettreY = tmp
-    print(lettreY<=len(square),  len(square[0])>=lettreX)
-    return square[lettreY][lettreX]
+    a = a.lower()
+    b = b.lower()
+    index = []
+    for columns, rows in enumerate(square):
+        if b in rows:
+            index.append(rows.index(b))
+        if a in rows:
+            index.append(columns)
+    print(index)
+    return square[index[0]] [index[1]] 
 
+def get_paires(text):
+    text = text.replace("\n", "").split(" ")
+    pairs = []
+
+    for i in range(len(text)//2):
+        for j in range(len(text[i])):
+            pairs.append((text[i][j], text[i+1][j]))
+
+        i+=1     
+    
+
+    return pairs     
+
+def replaceElement(old, new, l):
+    return [(e, new)[e==old] for e in l]    
 
 def uncypher(text, square, n):
-    text = "".join(text.split("\n"))
-    textList = list(map(list, text.split(" ")))[:-1]
-
-    print(textList)
-    decodeMSG = ""
     
-    print(decodeMSG)
+    paires = get_paires(text)
+
+    result = ""
+    i = 0
+    for paire in paires:
+        print(FoundLetters(paire[0], paire[1], square), paire)
+        result += FoundLetters(paire[0], paire[1], square)
+        
+        if i == 10:
+            break
+        i+= 1
+    
+    return result
+        
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
     
 
 
