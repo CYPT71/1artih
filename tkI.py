eb56ba473ed4458d53319ba149017840ff349cc4
OSoufian <osoufian@hotmail.fr>
2021-04-27 12:16:20 +0200

    Amélioration apparence de l'interface

--- tkI.py
+++ tkI.py
@@ -1,5 +1,6 @@
 from tkinter import Text, INSERT, Canvas, ttk, Tk
 from libsarit import TextCypher, makeSquare
+import tkinter.font
 
 root = Tk()
 
@@ -13,16 +14,16 @@ grid = ttk.Frame(root)
 
 # ---------------------------------------------------Widgets---------------------------------------------------#
 
-text_label = ttk.Label(base_text, text="Clear text >")
-key_entry = ttk.Label(key, text="Enter a text key >")
-length = ttk.Label(key, text="Enter a length >")
-result = ttk.Label(result_text, text="Result > ")
-grid_text = ttk.Label(grid, text="Cypher text")
+text_label = ttk.Label(base_text, text="Message à chiffrer ou à déchiffrer :")
+key_entry = ttk.Label(key, text="Entrer la clé : ")
+length = ttk.Label(key, text="Entrer la chaîne : ")
+result = ttk.Label(result_text, text="Message obtenu :")
+grid_text = ttk.Label(grid, text="La grille obtenue :")
 
-text_entry = Text(base_text, width=100, height=10)
+text_entry = Text(base_text, width=60, height=10)
 passWord = ttk.Entry(key, width=25)
 number = ttk.Entry(key, width=10)
-result_entry = Text(result_text, width=100, height=10)
+result_entry = Text(result_text, width=60, height=10)
 
 # ---------------------------------------------------Functions----------------------------------------------------------#
 
@@ -52,9 +53,9 @@ passWord.bind("<Motion>", generate_grid)
 
 #-------------------Fonctinalités principales---------------------#
 
-uncypherText = ttk.Button(buttons_interface, text="uncypher", command= lambda: text_entry.insert(INSERT, TextCypher(result_entry.get("1.0", "end"), passWord.get(), number.get(), True)))
-cypherText = ttk.Button(buttons_interface, text="cypher", command= lambda: result_entry.insert(INSERT, TextCypher(text_entry.get("1.0", "end"), passWord.get(), number.get(), False)))
-delete = ttk.Button(buttons_interface, text="delete", command= lambda: result_entry.delete("1.0", "end"))
+uncypherText = ttk.Button(buttons_interface, text="Déchiffrer", command= lambda: text_entry.insert(INSERT, TextCypher(result_entry.get("1.0", "end"), passWord.get(), number.get(), True)))
+cypherText = ttk.Button(buttons_interface, text="Chiffrer", command= lambda: result_entry.insert(INSERT, TextCypher(text_entry.get("1.0", "end"), passWord.get(), number.get(), False)))
+delete = ttk.Button(buttons_interface, text="Effacer", command= lambda: result_entry.delete("1.0", "end"))
 
 # ---------------------------------------------------Geometry Managers---------------------------------------------------#
 
@@ -64,22 +65,22 @@ buttons_interface.grid(row=2, column=0)
 result_text.grid(row=3, column=0)
 grid.grid(row=0, column=2, rowspan=3)
 
-text_label.grid(row=0, column=0)
+text_label.grid(row=0, column=0, sticky='w')
 key_entry.grid(row=0, column=0)
 length.grid(row=0, column=2)
-result.grid(row=0, column=0)
+result.grid(row=0, column=0, sticky='w')
 grid_text.grid(row=0, column=0)
 
-text_entry.grid(row=0, column=1, ipadx=10)
-passWord.grid(row=0, column=1)
+text_entry.grid(row=1, column=0, padx=5, pady=5)
+passWord.grid(row=0, column=1, padx=(0, 20))
 number.grid(row=0, column=3)
-result_entry.grid(row=0, column=1)
+result_entry.grid(row=4, column=0, padx=5, pady=5)
 
 cypherText.grid(row=0, column=0)
 uncypherText.grid(row=0, column=1)
 delete.grid(row=0, column=2)
 
-grille.grid(row=1, column=0)
+grille.grid(row=1, column=0, padx=10, pady=5)
 
 generate_grid(None)
 
