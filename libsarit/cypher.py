97c8082191b4a639a420f360a810d4c9dc6b966b
cyprien <cyprientaib1@gmail.com>
2021-05-03 15:33:35 +0200

    Debug Cypher

--- libsarit/cypher.py
+++ libsarit/cypher.py
@@ -1,6 +1,6 @@
 def encryption(letter, square):
     
-    return ("", "") if len(square) < 1 or len(square[0])<1 else [part for sub in test for i, l in enumerate(sub) if l ==letter for part in (sub[0], test[-1][i])]
+    return ("", "") if len(square) < 1 or len(square[0])<1 else [part for sub in square for i, l in enumerate(sub) if l ==letter for part in (sub[0], square[-1][i])]
     
 
 def cypher(text, square):
@@ -8,7 +8,7 @@ def cypher(text, square):
     for letter in text.replace("w", "vv"):
         if letter == " ":
             mainStr += (x +" "+ y+" ")            
-            x, y = ""
+            x, y = "", ""
         else:
             a, b = encryption(letter, square)
             x += a
