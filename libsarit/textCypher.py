c9994699f63fc2ad12f32900dc9f27c87b586299
cyprien <cyprientaib1@gmail.com>
2021-05-09 11:49:54 +0200

    Past function in private

--- libsarit/textCypher.py
+++ libsarit/textCypher.py
@@ -11,14 +11,14 @@ class TextCypher:
         self.key = makeSquare(key)
         self.cypher = is_cypher
 
-    def _text_cypher(self):
+    def __text_cypher(self):
         return cutter(cypher(self.text, self.key), 5).upper()
 
-    def _text_uncypher(self):
+    def __text_uncypher(self):
         return uncypher(self.text, self.key).upper()
 
     def __repr__(self):
         if text == "" or not spacer.isdigit() or key == "" or text is None or key is None:
             return ""
-        return self._text_uncypher() if self.cypher else self._text_cypher()
+        return self.__text_uncypher() if self.cypher else self.__text_cypher()
         
