ea803c09a1ad175d8b7b3dc0742177a305297258
cyprien <cyprientaib1@gmail.com>
2021-05-17 18:02:36 +0200

    Adding violance

--- libsarit/uncypher.py
+++ libsarit/uncypher.py
@@ -1,7 +1,12 @@
 def dencryption(a, b, square):
     i = -1
     while (i := i+1) < len(square):
-        if a.lower() in square[i] : return square[i][square[-1].index(b.lower())]
+        if a.lower() in square[i] : 
+            if b not in square[-1]:
+    
+                raise Exception("NTM tu as changé de clef")
+
+            return square[i][square[-1].index(b.lower())]
 
 def uncypher(text, square):
     if isinstance(text, str): text = text.split(" ")
@@ -17,6 +22,7 @@ def uncypher(text, square):
         else:
             x, y = text[0],text[1]
             for _ in range(2): text.pop(0)
-    
-    return "".join(dencryption(a,b, square) for a, b in zip(x,y)) + uncypher(text, square)
-
+    try:
+        return "".join(dencryption(a,b, square) for a, b in zip(x,y)) + uncypher(text, square)
+    except:
+        return "NTM tu as changé la clef tu veux que je te bute sérieux là"
