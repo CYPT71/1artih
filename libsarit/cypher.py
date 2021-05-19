9cf64f4e8cd77765df216742a9ea6f75b2f54bf2
cyprien <cyprientaib1@gmail.com>
2021-05-19 14:42:36 +0200

    Forget param

--- libsarit/cypher.py
+++ libsarit/cypher.py
@@ -3,5 +3,5 @@ def encryption(letter, square):
     for line in square: 
         if letter in line: return (line[0], square[-1][line.index(letter)])
 
-def cypher():
+def cypher(text, square):
     return " ".join(" ".join(map("".join,zip(*[cypherLetter(l, square) for l in s]))) for s in text.split(" "))
