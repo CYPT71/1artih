b7bb4f1207416e7a3736ec2176b7e0c2c7fe2880
cyprien <cyprientaib1@gmail.com>
2021-05-19 15:06:35 +0200

    Correct Cypher

--- libsarit/cypher.py
+++ libsarit/cypher.py
@@ -2,4 +2,4 @@ def encryption(letter, square):
     return [(line[0], square[-1][line.index(letter)]) for line in square if letter in line][0]
 
 def cypher(text, square):
-    return " ".join(" ".join(map("".join,zip(*[cypherLetter(l, square) for l in s]))) for s in text.split(" "))
+    return " ".join(" ".join(map("".join,zip(*[encryption(l, square) for l in s]))) for s in text.split(" "))
