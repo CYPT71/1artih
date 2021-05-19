9b55806a988947a49c91b11247d669e3e5369b4f
cyprien <cyprientaib1@gmail.com>
2021-05-19 15:04:22 +0200

    Reduce Cypher

--- libsarit/cypher.py
+++ libsarit/cypher.py
@@ -1,6 +1,5 @@
 def encryption(letter, square):    
-    for line in square: 
-        if letter in line: return (line[0], square[-1][line.index(letter)])
+    return [(line[0], square[-1][line.index(letter)]) for line in square if letter in line][0]
 
 def cypher(text, square):
     return " ".join(" ".join(map("".join,zip(*[cypherLetter(l, square) for l in s]))) for s in text.split(" "))
