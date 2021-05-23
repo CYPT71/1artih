5141d92a925e95cbe91a028b522f9bef35d18a72
cyprien <cyprientaib1@gmail.com>
2021-05-23 17:16:05 +0200

    Micro Opti

--- libsarit/functionalPart.py
+++ libsarit/functionalPart.py
@@ -1,19 +1,16 @@
 import string, unicodedata, re
 from textwrap import wrap
-
+regex = re.compile("[^A-Za-z]+")
 def remove_transform(text):
     # replace accentuted word
     text = unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("utf8")
     # return only letter in lower case 
-    return re.sub('[^A-Za-z]+', '', text).lower().replace("w", "v")
+    return re.sub(regex, '', text).lower().replace("w", "v")
 
 
 def make_square(key):
     # ord and extract letters 
-    key = remove_transform(key)
-    letters = (letter_key := sorted(set(key),key=key.index)) +\
-                [l for l in string.ascii_lowercase if l not in letter_key and l != "w"]
-
+    letters = (letter_key := "".join(sorted(set((key := remove_transform(key))),key=key.index))) + string.ascii_lowercase.strip(letter_key)
     return [letters[i:i+5] for i in range(0, len(letters), 5)]
 
 def cutter(text, n):
--- libsarit/uncypher.py
+++ libsarit/uncypher.py
@@ -10,17 +10,16 @@ def decryption(a, b, square):
 def uncypher(text, square):
     if isinstance(text, str): text = text.split(" ")
     if text == []: return ""
-    
-    if len(text)==1:
+
+    elif len(text)==1:
         t=  text[0]
         text, x, y = [], t[:len(t)//2], t[len(t)//2:]
+    elif len(text[0]) != len(text[1]):
+      t= "".join(text)
+      text, x, y = [], t[:len(t)//2], t[len(t)//2:]
     else:
-        if len(text[0]) != len(text[1]):
-            t= "".join(text)
-            text, x, y = [], t[:len(t)//2], t[len(t)//2:]
-        else:
-            x, y = text[0],text[1]
-            for _ in range(2): text.pop(0)
+       x, y = text[0],text[1]
+       for _ in range(2): text.pop(0)
     try:
         return "".join(decryption(a,b, square) for a, b in zip(x,y)) + uncypher(text, square)
     except ValueError as e:
