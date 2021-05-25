449d2d9779c94447c6431326e7428c086a12cb8d
cyp t <cyprientaib1@gmail.com>
2021-05-25 19:20:46 +0200

    Merde

--- libsarit/functionalPart.py
+++ libsarit/functionalPart.py
@@ -5,7 +5,7 @@ def remove_transform(text):
     # replace accentuted word
     text = unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("utf8")
     # return only letter in lower case
-    return re.sub(regex, '', text.replace("w", "v")).lower()
+    return re.sub(regex, '', text).lower().replace("w", "")
 
 
 def make_square(key):
@@ -14,6 +14,7 @@ def make_square(key):
 
     letters = (letter_key := sorted(set(key),key=key.index)) + [l for l in string.ascii_lowercase if l not in letter_key and l != "w"]
 
+    print([letters[i:i+5] for i in range(0, len(letters), 5)])
     return [letters[i:i+5] for i in range(0, len(letters), 5)]
 
 def cutter(text, n):
